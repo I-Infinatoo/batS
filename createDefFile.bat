bc07aa996aea387e12f30063a0f3a9909a8e42db
Infinatoo <ishurajan234@gmail.com>
2022-10-08 00:24:32 +0530

    sync changes

--- createDefFile.bat
+++ createDefFile.bat
@@ -1,8 +1,3 @@
-<<<<<<< HEAD
 @echo off
 echo 30 > values.txt
-=======
-@echo off
-echo 30 > values.txt
->>>>>>> cf25f5d (sync)
 echo 90 >> values.txt
--- startup.bat
+++ startup.bat
@@ -1,6 +1,2 @@
 @echo off
-<<<<<<< HEAD
 start /min "newBattery" C:\Users\ishu1\OneDrive\Desktop\battery\newBattery.exe
-=======
-start /min "newBattery" C:\Users\ishu1\OneDrive\Desktop\battery\newBattery.exe
->>>>>>> cf25f5d (sync)
